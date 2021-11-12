b6274a6a8817ef8fbc190d0ce8b11b40a0f169ba
fredi <fredibagusk@gmail.com>
2021-11-13 06:42:45 +0700

    Slicing Splash Page

---
+++ terbangin/lib/ui/pages/splash_page.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+// ignore: unused_import
+import 'package:terbangin/shared/theme.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kPrimaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              width: 100,
+              height: 100,
+              margin: const EdgeInsets.only(bottom: 50),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    'assets/icon_plane.png',
+                  ),
+                ),
+              ),
+            ),
+            Text(
+              'TERBANGIN',
+              style: whiteTextStyle.copyWith(
+                fontSize: 32,
+                fontWeight: medium,
+                letterSpacing: 10,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
