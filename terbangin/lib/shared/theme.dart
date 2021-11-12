739d969c244c8394f6e7747c8d502326c71c7015
fredi <fredibagusk@gmail.com>
2021-11-13 05:59:19 +0700

    Menambahkan Tema dan Shared Variable

---
+++ terbangin/lib/shared/theme.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color kPrimaryColor = const Color(0xff055052);
+Color kBlackColor = const Color(0xff1F1449);
+Color kWhiteColor = const Color(0xffFFFFFF);
+Color kGreyColor = const Color(0xff9698A9);
+Color kGreenColor = const Color(0xff0EC3AE);
+Color kRedColor = const Color(0xffEB70A5);
+Color kBackgroundColor = const Color(0xffFAFAFA);
+Color kInactiveColor = const Color(0xffDBD7EC);
+
+TextStyle blackTextStyle = GoogleFonts.ptSans(
+  color: kBlackColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.ptSans(
+  color: kWhiteColor,
+);
+TextStyle greyTextStyle = GoogleFonts.ptSans(
+  color: kGreyColor,
+);
+TextStyle greenTextStyle = GoogleFonts.ptSans(
+  color: kGreenColor,
+);
+TextStyle redTextStyle = GoogleFonts.ptSans(
+  color: kRedColor,
+);
+TextStyle primaryTextStyle = GoogleFonts.ptSans(
+  color: kPrimaryColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w600;
+FontWeight bold = FontWeight.w700;
+FontWeight extrabold = FontWeight.w800;
+FontWeight black = FontWeight.w900;
