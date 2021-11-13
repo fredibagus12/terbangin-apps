9c4639df85db157f5f358e5ecef3b746ac5485ce
fredi <fredibagusk@gmail.com>
2021-11-13 18:10:38 +0700

    Slicing Home - Bagian Popular Destinations

--- terbangin/lib/ui/pages/home_page.dart
+++ terbangin/lib/ui/pages/home_page.dart
@@ -61,9 +61,100 @@ class HomePage extends StatelessWidget {
       );
     }
 
+    Widget popularDestinations() {
+      return Container(
+        // ignore: prefer_const_constructors
+        margin: EdgeInsets.only(top: 30),
+        child: Row(
+          children: [
+            Container(
+              width: 200,
+              height: 323,
+              margin: EdgeInsets.only(
+                left: defaultMargin,
+              ),
+              // ignore: prefer_const_constructors
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(18),
+                color: kWhiteColor,
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    width: 180,
+                    height: 220,
+                    // ignore: prefer_const_constructors
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(18),
+                      // ignore: prefer_const_constructors
+                      image: DecorationImage(
+                        // ignore: prefer_const_constructors
+                        image: AssetImage(
+                          'assets/image_destination_1.png',
+                        ),
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.topRight,
+                      child: Container(
+                        width: 55,
+                        height: 30,
+                        // ignore: prefer_const_constructors
+
+                        decoration: BoxDecoration(
+                          color: kWhiteColor,
+                          // ignore: prefer_const_constructors
+
+                          // ignore: prefer_const_constructors
+                          borderRadius: BorderRadius.only(
+                            // ignore: prefer_const_constructors
+                            bottomLeft: Radius.circular(18),
+                          ),
+                        ),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 20,
+                              height: 20,
+                              // ignore: prefer_const_constructors
+                              margin: EdgeInsets.only(right: 2),
+                              // ignore: prefer_const_constructors
+                              decoration: BoxDecoration(
+                                // ignore: prefer_const_constructors
+                                image: DecorationImage(
+                                  // ignore: prefer_const_constructors
+                                  image: AssetImage(
+                                    'assets/star.png',
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Text(
+                              '4,8',
+                              style: blackTextStyle.copyWith(
+                                fontWeight: medium,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
     return ListView(
       children: [
         header(),
+        popularDestinations(),
       ],
     );
   }
