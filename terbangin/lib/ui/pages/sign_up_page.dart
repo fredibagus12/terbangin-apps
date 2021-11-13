0c619981f2c5dc7b491691e69fb8bc172c667546
fredi <fredibagusk@gmail.com>
2021-11-13 09:14:49 +0700

    Slicing Sign Up - Input Section 1

--- terbangin/lib/ui/pages/sign_up_page.dart
+++ terbangin/lib/ui/pages/sign_up_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_element, duplicate_ignore
+
 import 'package:flutter/material.dart';
 import 'package:terbangin/shared/theme.dart';
 
@@ -5,6 +7,7 @@ class SignUpPage extends StatelessWidget {
   const SignUpPage({Key? key}) : super(key: key);
 
   @override
+  // ignore: duplicate_ignore
   Widget build(BuildContext context) {
     // ignore: unused_element
     Widget title() {
@@ -21,6 +24,65 @@ class SignUpPage extends StatelessWidget {
       );
     }
 
+    Widget inputSection() {
+      Widget emailInput() {
+        return Container(
+          margin: const EdgeInsets.only(bottom: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // ignore: prefer_const_constructors
+              Text(
+                'Nama Lengkap',
+              ),
+              // ignore: prefer_const_constructors
+              SizedBox(
+                height: 6,
+              ),
+              TextFormField(
+                cursorColor: kBlackColor,
+                decoration: InputDecoration(
+                  hintText: 'Nama Lengkapmu',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(
+                      defaultRadius,
+                    ),
+                    borderSide: BorderSide(
+                      color: kPrimaryColor,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+
+      return Container(
+        margin: const EdgeInsets.only(top: 30),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 30,
+        ),
+        decoration: BoxDecoration(
+          color: kWhiteColor,
+          borderRadius: BorderRadius.circular(
+            defaultRadius,
+          ),
+        ),
+        child: Column(
+          children: [
+            emailInput(),
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
       backgroundColor: kBackgroundColor,
       body: SafeArea(
@@ -30,6 +92,7 @@ class SignUpPage extends StatelessWidget {
           ),
           children: [
             title(),
+            inputSection(),
           ],
         ),
       ),
